584761be49b8982bea834b596c97eaff7e3f9a6b
hybong <hongyibong@gmail.com>
2024-12-02 18:01:35 +0800

    Implemented left and right movement of userplane

--- src/main/java/com/example/demo/Levels/LevelParent.java
+++ src/main/java/com/example/demo/Levels/LevelParent.java
@@ -113,13 +113,16 @@ public abstract class LevelParent extends Observable {
 				KeyCode kc = e.getCode();
 				if (kc == KeyCode.UP) user.moveUp();
 				if (kc == KeyCode.DOWN) user.moveDown();
+				if (kc == KeyCode.LEFT) user.moveLeft();
+				if (kc == KeyCode.RIGHT) user.moveRight();
 				if (kc == KeyCode.SPACE) fireProjectile();
 			}
 		});
 		background.setOnKeyReleased(new EventHandler<KeyEvent>() {
 			public void handle(KeyEvent e) {
 				KeyCode kc = e.getCode();
-				if (kc == KeyCode.UP || kc == KeyCode.DOWN) user.stop();
+				if (kc == KeyCode.UP || kc == KeyCode.DOWN) user.stopVertically();
+				if (kc == KeyCode.LEFT || kc == KeyCode.RIGHT) user.stopHorizontally();
 			}
 		});
 		root.getChildren().add(background);
--- src/main/java/com/example/demo/models/UserPlane.java
+++ src/main/java/com/example/demo/models/UserPlane.java
@@ -7,28 +7,41 @@ public class UserPlane extends FighterPlane {
 	private static final String IMAGE_NAME = "userplane.png";
 	private static final double Y_UPPER_BOUND = -40;
 	private static final double Y_LOWER_BOUND = 600.0;
+	private static final double X_UPPER_BOUND = -1000;
+	private static final double X_LOWER_BOUND = 1000.0;
 	private static final double INITIAL_X_POSITION = 5.0;
 	private static final double INITIAL_Y_POSITION = 300.0;
 	private static final int IMAGE_HEIGHT = 150;
 	private static final int VERTICAL_VELOCITY = 8;
+	private static final int HORIZONTAL_VELOCITY = 8;
 	private static final int PROJECTILE_X_POSITION = 110;
 	private static final int PROJECTILE_Y_POSITION_OFFSET = 20;
-	private int velocityMultiplier;
+	private int verticalVelocityMultiplier;
+	private int horizontalVelocityMultiplier;
 	private int numberOfKills;
 
 	public UserPlane(int initialHealth) {
 		super(IMAGE_NAME, IMAGE_HEIGHT, INITIAL_X_POSITION, INITIAL_Y_POSITION, initialHealth);
-		velocityMultiplier = 0;
+		verticalVelocityMultiplier = 0;
+		horizontalVelocityMultiplier = 0;
 	}
 	
 	@Override
 	public void updatePosition() {
-		if (isMoving()) {
+		if (isMovingVertically()) {
 			double initialTranslateY = getTranslateY();
-			this.moveVertically(VERTICAL_VELOCITY * velocityMultiplier);
-			double newPosition = getLayoutY() + getTranslateY();
-			if (newPosition < Y_UPPER_BOUND || newPosition > Y_LOWER_BOUND) {
+			this.moveVertically(VERTICAL_VELOCITY * verticalVelocityMultiplier);
+			double newYPosition = getLayoutY() + getTranslateY();
+			if (newYPosition < Y_UPPER_BOUND || newYPosition > Y_LOWER_BOUND) {
 				this.setTranslateY(initialTranslateY);
+			}
+		}
+		if (isMovingHorizontally()) {
+			double initialTranslateX = getTranslateX();
+			this.moveHorizontally(HORIZONTAL_VELOCITY * horizontalVelocityMultiplier);
+			double newXPosition = getLayoutX() + getTranslateX();
+			if (newXPosition < X_UPPER_BOUND || newXPosition > X_LOWER_BOUND) {
+				this.setTranslateX(initialTranslateX);
 			}
 		}
 	}
@@ -43,20 +56,35 @@ public class UserPlane extends FighterPlane {
 		return new UserProjectile(PROJECTILE_X_POSITION, getProjectileYPosition(PROJECTILE_Y_POSITION_OFFSET));
 	}
 
-	private boolean isMoving() {
-		return velocityMultiplier != 0;
+	private boolean isMovingVertically() {
+		return verticalVelocityMultiplier != 0;
+	}
+
+	private boolean isMovingHorizontally() {
+		return horizontalVelocityMultiplier != 0;
 	}
 
 	public void moveUp() {
-		velocityMultiplier = -1;
+		verticalVelocityMultiplier = -1;
 	}
 
 	public void moveDown() {
-		velocityMultiplier = 1;
+		verticalVelocityMultiplier = 1;
 	}
 
-	public void stop() {
-		velocityMultiplier = 0;
+	public void moveLeft() {
+		horizontalVelocityMultiplier = -1;
+	}
+
+	public void moveRight() {
+		horizontalVelocityMultiplier = 1;
+	}
+
+	public void stopVertically() {
+		verticalVelocityMultiplier = 0;
+	}
+	public void stopHorizontally() {
+		horizontalVelocityMultiplier = 0;
 	}
 
 	public int getNumberOfKills() {
