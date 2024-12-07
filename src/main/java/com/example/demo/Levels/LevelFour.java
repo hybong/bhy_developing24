bc05c34dafd37f83370bfc0a4c7ac92334b12929
hybong <hongyibong@gmail.com>
2024-12-08 02:34:20 +0800

    completed level four TODO: hide health of destroyed boss

--- src/main/java/com/example/demo/Levels/LevelFour.java
+++ src/main/java/com/example/demo/Levels/LevelFour.java
@@ -1,20 +1,23 @@
 package com.example.demo.Levels;
 
-import com.example.demo.models.Boss;
 import com.example.demo.Levels.levelView.LevelView;
 import com.example.demo.Levels.levelView.LevelViewLevelFour;
+import com.example.demo.models.Boss;
+import com.example.demo.models.secondBoss;
 import javafx.scene.Scene;
 
 public class LevelFour extends LevelParent {
 
     private static final String BACKGROUND_IMAGE_NAME = "/com/example/demo/images/background4.jpg";
-    private static final int PLAYER_INITIAL_HEALTH = 5;
-    private final Boss boss;
+    private static final int PLAYER_INITIAL_HEALTH = 8;
+    private final Boss bossOne;
+    private final secondBoss bossTwo;
     private LevelViewLevelFour levelView;
 
     public LevelFour(double screenHeight, double screenWidth) {
         super(BACKGROUND_IMAGE_NAME, screenHeight, screenWidth, PLAYER_INITIAL_HEALTH);
-        boss = new Boss();
+        bossOne = new Boss();
+        bossTwo = new secondBoss();
     }
 
     @Override
@@ -27,7 +30,7 @@ public class LevelFour extends LevelParent {
         if (userIsDestroyed()) {
             loseGame();
         }
-        else if (boss.isDestroyed()) {
+        else if (bossOne.isDestroyed() && bossTwo.isDestroyed()) {
             winGame();
         }
     }
@@ -35,13 +38,14 @@ public class LevelFour extends LevelParent {
     @Override
     protected void spawnEnemyUnits() {
         if (getCurrentNumberOfEnemies() == 0) {
-            addEnemyUnit(boss);
+            addEnemyUnit(bossOne);
+            addEnemyUnit(bossTwo);
         }
     }
 
     @Override
     protected LevelView instantiateLevelView() {
-        levelView = new LevelViewLevelFour(getRoot(), PLAYER_INITIAL_HEALTH);
+        levelView = new LevelViewLevelFour(getRoot(), PLAYER_INITIAL_HEALTH, Boss.HEALTH, secondBoss.HEALTH);
         return levelView;
     }
 
@@ -49,20 +53,23 @@ public class LevelFour extends LevelParent {
     public Scene initializeScene() {
         Scene scene = super.initializeScene();
         levelView.displayShield();
+        levelView.displayBossHealth();
         return scene;
     }
 
     public void updateLevelView() {
         super.updateLevelView();
         // update shield position
-        levelView.updateShieldPosition(boss);
+        levelView.updateShieldPosition(bossOne, bossTwo);
 //		boolean bossWasShielded = isBossShielded;
 
-        if (boss.isShielded()) {
-            levelView.showShield();
-        } else {
-            levelView.hideShield();
-        }
+        if (bossOne.isShielded()) levelView.showShieldOne();
+        else levelView.hideShieldOne();
+        if (bossTwo.isShielded()) levelView.showShieldTwo();
+        else levelView.hideShieldTwo();
+
+        levelView.updateHealthPosition(bossOne, bossTwo);
+        levelView.updateBossHealth(bossOne.getHealth(), bossTwo.getHealth());
 
     }
 
--- src/main/java/com/example/demo/Levels/levelView/LevelViewLevelFour.java
+++ src/main/java/com/example/demo/Levels/levelView/LevelViewLevelFour.java
@@ -1,5 +1,7 @@
 package com.example.demo.Levels.levelView;
 
+import com.example.demo.models.secondBoss;
+import com.example.demo.view.BossHealth;
 import com.example.demo.view.ShieldImage;
 import com.example.demo.models.Boss;
 import javafx.scene.Group;
@@ -7,30 +9,65 @@ import javafx.scene.Group;
 public class LevelViewLevelFour extends LevelView {
 
     private final Group root;
-    private final ShieldImage shieldImage;
+    private final ShieldImage shieldImageOne;
+    private final ShieldImage shieldImageTwo;
+    private final BossHealth bossOneHealth;
+    private final BossHealth bossTwoHealth;
 
-    public LevelViewLevelFour(Group root, int heartsToDisplay) {
+    public LevelViewLevelFour(Group root, int heartsToDisplay, int bossOneHealth, int bossTwoHealth) {
         super(root, heartsToDisplay);
         this.root = root;
-        this.shieldImage = new ShieldImage();
+        this.shieldImageOne = new ShieldImage();
+        this.shieldImageTwo = new ShieldImage();
+        this.bossOneHealth = new BossHealth(bossOneHealth);
+        this.bossTwoHealth = new BossHealth(bossTwoHealth);
     }
 
     public void displayShield() {
-        root.getChildren().add(shieldImage);
+        root.getChildren().addAll(shieldImageOne, shieldImageTwo);
     }
 
-    public void updateShieldPosition(Boss boss) {
-        double bossX = boss.getLayoutX() + boss.getTranslateX();
-        double bossY = boss.getLayoutY() + boss.getTranslateY();
-        shieldImage.updateShieldPosition(bossX, bossY);
+    public void updateShieldPosition(Boss bossOne, secondBoss bossTwo) {
+        double bossOneX = bossOne.getLayoutX() + bossOne.getTranslateX();
+        double bossOneY = bossOne.getLayoutY() + bossOne.getTranslateY();
+        shieldImageOne.updateShieldPosition(bossOneX, bossOneY);
+        double bossTwoX = bossTwo.getLayoutX() + bossTwo.getTranslateX();
+        double bossTwoY = bossTwo.getLayoutY() + bossTwo.getTranslateY();
+        shieldImageTwo.updateShieldPosition(bossTwoX, bossTwoY);
     }
 
-    public void showShield() {
-        shieldImage.showShield();
+    public void showShieldOne() {
+        shieldImageOne.showShield();
     }
 
-    public void hideShield() {
-        shieldImage.hideShield();
+    public void hideShieldOne() {
+        shieldImageOne.hideShield();
+    }
+
+    public void showShieldTwo() {
+        shieldImageTwo.showShield();
+    }
+
+    public void hideShieldTwo() {
+        shieldImageTwo.hideShield();
+    }
+
+    public void updateHealthPosition(Boss bossOne, secondBoss bossTwo) {
+        double bossOneX = bossOne.getLayoutX() + bossOne.getTranslateX();
+        double bossOneY = bossOne.getLayoutY() + bossOne.getTranslateY();
+        bossOneHealth.updateHealthPosition(bossOneX, bossOneY);
+        double bossTwoX = bossTwo.getLayoutX() + bossTwo.getTranslateX();
+        double bossTwoY = bossTwo.getLayoutY() + bossTwo.getTranslateY();
+        bossTwoHealth.updateHealthPosition(bossTwoX, bossTwoY);
+    }
+
+    public void updateBossHealth(int presentHealthOne, int presentHealthTwo) {
+        bossOneHealth.updateHealth(presentHealthOne);
+        bossTwoHealth.updateHealth(presentHealthTwo);
+    }
+
+    public void displayBossHealth() {
+        root.getChildren().addAll(bossOneHealth, bossTwoHealth);
     }
 
 }
