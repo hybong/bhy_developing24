457fb2b8bde571622eb6d050d2c10df7011a8679
hybong <hongyibong@gmail.com>
2024-12-12 02:56:00 +0800

    added fighter plane destroy sound effect

--- src/main/java/com/example/demo/models/FighterPlane.java
+++ src/main/java/com/example/demo/models/FighterPlane.java
@@ -1,12 +1,18 @@
 package com.example.demo.models;
 
+import com.example.demo.media.SoundEffect;
+
 public abstract class FighterPlane extends ActiveActorDestructible {
 
 	public int health;
+	private static final String DESTROY_SOUND_PATH = "/com/example/demo/media/soundEffects/destroyed/fighterPlaneDestroy.mp3";
+	private final SoundEffect destroySound;
+	private final double DESTROY_SOUND_VOLUME = 0.5;
 
 	public FighterPlane(String imageName, int imageHeight, double initialXPos, double initialYPos, int health) {
 		super(imageName, imageHeight, initialXPos, initialYPos);
 		this.health = health;
+		destroySound = new SoundEffect(DESTROY_SOUND_PATH);
 	}
 
 	public abstract ActiveActorDestructible fireProjectile();
@@ -15,6 +21,7 @@ public abstract class FighterPlane extends ActiveActorDestructible {
 	public void takeDamage() {
 		health--;
 		if (healthAtZero()) {
+			destroySound.playSoundEffect(DESTROY_SOUND_VOLUME);
 			this.destroy();
 		}
 	}
