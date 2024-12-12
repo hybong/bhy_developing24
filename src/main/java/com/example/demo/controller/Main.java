47cecdf39310b4c24be9a755a8e2e03a4bc3eda7
hybong <hongyibong@gmail.com>
2024-12-12 18:11:44 +0800

    modified screen width

--- src/main/java/com/example/demo/controller/Main.java
+++ src/main/java/com/example/demo/controller/Main.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-	public static final int SCREEN_WIDTH = 1300;
+	public static final int SCREEN_WIDTH = 1200;
 	public static final int SCREEN_HEIGHT = 750;
 	public static final String TITLE = "Sky Battle";
 	private MainMenu myMainMenu;
