ec3519bf498abd9f65654e2c9fd477b6ef941932
hybong <hongyibong@gmail.com>
2024-12-12 19:41:55 +0800

    adjusted image position

--- src/main/java/com/example/demo/view/GameOverImage.java
+++ src/main/java/com/example/demo/view/GameOverImage.java
@@ -8,7 +8,7 @@ public class GameOverImage extends ImageView {
 
 	private static final double WIDTH = 300;
 	private static final double XPosition = (double) Main.SCREEN_WIDTH /2 - WIDTH /2;
-	private static final double YPosition = (double) Main.SCREEN_HEIGHT /2 - WIDTH /2;
+	private static final double YPosition = (double) Main.SCREEN_HEIGHT * 0.2;
 	private static final String GAME_OVER_IMAGE = "/com/example/demo/images/gameover.png";
 
 	public GameOverImage() {
